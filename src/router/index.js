955c6ee507fce5fe8bb4cc613ed8bf8b4f56dd2f
Sanja Žitnik <153129371+Roundpidge@users.noreply.github.com>
2025-08-11 21:40:35 +0200

    Prepravljen index.js
    dodano: 
    -import NotFound from '@/views/NotFound.vue'
    -{
            path: '/:pathMatch(.*)*',
            name: 'NotFound',
            component: NotFound
        },
    -import { auth } from '@/firebase.js' i 
    
    router.beforeEach((to) => {
        const isAuthenticated = !!auth.currentUser
        if (!isAuthenticated && to.name === 'korisnik') {
            return { name: 'log' }
        }
    }) Zgodno za preusmjeravanje korisnika na prijavu
    
    izmjene:                                                                                    import korisnik from '@/views/prijavljenKorisnik.vue' u dinamičku rutu-->  
          {
            path: '/korisnik/:email',
            name: 'korisnik',
            component: korisnik
        },

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { auth } from '@/firebase.js'
+
+
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
 import reg from '@/views/Register.vue'
@@ -7,6 +10,7 @@ import products from '@/views/products.vue'
 import tretmani from '@/views/tretmani.vue'
 import korisnik from '@/views/prijavljenKorisnik.vue'
 import logout from '@/views/logout.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const routes = [
     { path: '/', component: Home },
@@ -16,7 +20,6 @@ const routes = [
     { path: '/logout', component: logout },
     { path: '/products', component: products },
     { path: '/tretmani', component: tretmani },
-    { path: '/korisnik', component: korisnik },
 
     {
         path: '/proizvodDetaljno/:id',
@@ -32,11 +35,31 @@ const routes = [
         component: () => import('@/views/tretmaniDetails.vue'),
         props: true
     },
+
+    {
+        path: '/korisnik/:email',
+        name: 'korisnik',
+        component: korisnik
+    },
+
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: NotFound
+    },
+
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to) => {
+    const isAuthenticated = !!auth.currentUser
+    if (!isAuthenticated && to.name === 'korisnik') {
+        return { name: 'log' }
+    }
 })
 
 export default router
