760143bb0fa4c8eccbd45c65628584b2b49dac73
Sanja Žitnik <153129371+Roundpidge@users.noreply.github.com>
2025-08-11 21:29:40 +0200

    Prepravljen  prijavljenKorisnik.vue
                                                                                                     Dodano:                                                                                                          -import { useRoute } from 'vue-router';
    -const route = useRoute()
                                                                                                             Izmjene:
     <h1>Korisnik: {{  }}</h1>--> <h1>Korisnik: {{ route.params.email }}</h1> prikaz emaila pojedinog prijavljenog korisnika uz pomoć route.params.email koji nam je omogućio import useRoute i const route=use Route()

--- src/views/prijavljenKorisnik.vue
+++ src/views/prijavljenKorisnik.vue
@@ -1,16 +1,18 @@
 <script setup>
 
 import { ref } from 'vue'
-
+import { useRoute } from 'vue-router';
 const placeholderREF=ref(true)
 
 const stilInputa=ref('border-blue-600 border-2 rounded-lg bg-blue-200 mb-3 p-1 flex flex-col items-center');
 const stilForme=ref('inline-block border-blue-600 bg-white border-4 rounded-lg mt-2 p-2');
+
+const route = useRoute()
 </script>
 
 <template>
     
-    <h1>Korisnik: {{  }}</h1>
+    <h1>Korisnik: {{ route.params.email }}</h1>
     <img src="@/assets/placeholder.jpeg"></img>
     <hr></hr>
     <div v-if="placeholderREF">Da Da da</div>
