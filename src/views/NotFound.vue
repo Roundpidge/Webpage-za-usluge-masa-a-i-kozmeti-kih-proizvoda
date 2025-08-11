11071b5010d31f6415570d33828354a7656a68ca
Sanja Žitnik <153129371+Roundpidge@users.noreply.github.com>
2025-08-11 21:43:43 +0200

    Add files via upload
    Praktički služi kao vizualni prikazi Error 404 koji koristi sliku NotFoundImage.jpg iz mape assets koja se nalazi unutar mape src

---
+++ src/views/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+ <h1> Stranica {{ $route.path }} ne postoji! </h1>
+ <img src="@/assets/NotFoundImage.jpg" class="h-100"></img>
+  <RouterLink to="/" class="hover:text-orange-600 underline">Natrag na početnu stranicu</RouterLink>
+</template>
