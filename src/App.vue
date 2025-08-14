6a1c5f56ba8333bf38015bdfb0657a48c5a6f112
Sanja Žitnik <153129371+Roundpidge@users.noreply.github.com>
2025-08-14 21:58:40 +0200

    Mini Update App.vue
    Preimenovan path register-->registerLogin za RouterLink i naziv RouterLinka Registracija-->Registracija i prijava kao i brisanje routerlink za Login i gumb za odjavu.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import {ref} from 'vue'
 import { RouterLink, RouterView, useRouter } from 'vue-router'
 import gumb from './components/gumb.vue'
 import backgroundImg from '@/assets/Backround.jpg' 
-import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '@/firebase.js'
 
 const router = useRouter()
@@ -17,39 +17,28 @@ onAuthStateChanged(auth, (currentUser) => {
  }
 });
 
-const logout = async () => {
-  try {
-    await signOut(auth)
-    router.replace('/logout') 
-  } catch (err) {
-    console.error('Greška pri odjavi:', err)
-  }
-}
-
 </script>
 
 <template>
   
   <div class="min-h-screen flex flex-col bg-cover bg-center" :style="{ backgroundImage: `url(${backgroundImg})` }">
 
-        <div class="absolute inset-0 bg-white/50 "></div>
-    <nav class="relative flex gap-4 p-4 bg-blue-400">
- <span v-if="!user" class="text-rose-600 text-sm"> Nema prijavljenog korisnika! </span>
+        <div class="absolute inset-0 bg-white/65 "></div>
+    <nav class="relative flex gap-4 p-4 bg-[#e0fad9]">
+ <span v-if="!user" class="text-[#9a655a] text-sm"> Nema prijavljenog korisnika! </span>
  <span v-else class="text-emerald-600 text-sm"> Prijavljen korisnik: <b>{{ user.email }}</b></span>
       <RouterLink to="/"><gumb>Početna</gumb></RouterLink>
       <RouterLink to="/about"><gumb>O nama</gumb></RouterLink>
       <RouterLink to="/products"><gumb>Proizvodi</gumb></RouterLink>
       <RouterLink to="/tretmani"><gumb>Tretmani</gumb></RouterLink>
-      <RouterLink to="/register"><gumb>Registracija</gumb></RouterLink>
-      <RouterLink to="/login"><gumb>Prijava</gumb></RouterLink>
-      <button @click.prevent="logout"><gumb>Odjava</gumb></button>
+      <RouterLink to="/registerLogin"><gumb>Registracija i prijava</gumb></RouterLink>
     </nav>
 
     <main class="relative flex-grow">
       <RouterView />
     </main>
 
-    <footer class="relative bg-gradient-to-r from-green-200 via-orange-200 to-green-200  text-center p-4 mt-8">
+    <footer class="relative bg-[#c2e9b7]  text-center p-4 mt-8">
       © 2025 No Stress Kozmetički Salon
     </footer>
   </div>
