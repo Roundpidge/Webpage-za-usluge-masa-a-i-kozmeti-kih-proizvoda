0bf6758e9b1097071cf32204925139b31a641ded
Sanja Žitnik <153129371+Roundpidge@users.noreply.github.com>
2025-08-25 13:30:50 +0200

    Update App.vue
    dodan const isAdmin=ref(false);-->treba nam kao "semafor" za admin ili korisnički račun
    const {uloga}=useUserStore();
    
    dodani: 
    const checkRole = async()=> {
       const uloga = await nadiUlogu(); 
       if(uloga.value==="admin"){
        return isAdmin.value=true; -->zeleno svijetlo semafora isAdmin
       }
       else{
        return isAdmin.value=false -->crveno svijetlo
       }
    } -->provjerava ulogu korisnika, služi da bi se kasnije uz pomoću v-if utvrdilo koji routerlink prikazatina temelju isAdmin vrijednosti
    
    
    <RouterLink v-if="user && !checkRole" :to="{ name: 'korisnik', params: {email: user.email} }"><gumb>Korisnicni racun</gumb></RouterLink>
    <RouterLink v-if="user && checkRole" :to="{ name: 'adminPage', params: {email: user.email} }"><gumb>Admin racun</gumb></RouterLink> --> na temelju isAdmin vrijednosti i usera utvrđuje se koja će se stranica prikazati, ako je korisnik prijavljen(user!=null) i uloga mu je očitana kao admin(isAdmin=true), prikazat će mu se tokom prijave routerlink(gumbić) za stranicu admin račun, a ako je prijavljen i nema ulogu admina onda može ući samo na routerlink(gumbić) za stranicu korisnički račun, u suprotnom se nijedna stranica ne prikazuje.

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,32 @@ import gumb from './components/gumb.vue'
 import backgroundImg from '@/assets/Backround.jpg' 
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '@/firebase.js'
+import { useUserStore } from './stores/projektStore.js';
 
 const router = useRouter()
+const isAdmin=ref(false);
+const user = ref(null);
 
-const user = ref(null)
 onAuthStateChanged(auth, (currentUser) => {
  if (currentUser) {
  user.value = currentUser;
+
  } else {
  user.value = null;
  }
 });
 
+const {uloga}=useUserStore();
+
+const checkRole = async()=> {
+   const uloga = await nadiUlogu(); 
+   if(uloga.value==="admin"){
+    return isAdmin.value=true;
+   }
+   else{
+    return isAdmin.value=false
+   }
+}
 </script>
 
 <template>
@@ -32,7 +46,8 @@ onAuthStateChanged(auth, (currentUser) => {
       <RouterLink to="/products"><gumb>Proizvodi</gumb></RouterLink>
       <RouterLink to="/tretmani"><gumb>Tretmani</gumb></RouterLink>
       <RouterLink to="/registerLogin"><gumb>Registracija i prijava</gumb></RouterLink>
-      <RouterLink v-if="user" :to="{ name: 'korisnik', params: {email: user.email} }"><gumb>Korisnicni racun</gumb></RouterLink>
+      <RouterLink v-if="user && !checkRole" :to="{ name: 'korisnik', params: {email: user.email} }"><gumb>Korisnicni racun</gumb></RouterLink>
+      <RouterLink v-if="user && checkRole" :to="{ name: 'adminPage', params: {email: user.email} }"><gumb>Admin racun</gumb></RouterLink>
     </nav>
 
     <main class="relative flex-grow">
