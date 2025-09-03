2d92f8e6c5fec70eddf6a1e419f9bf5a4c8267a2
Sanja Žitnik <153129371+Roundpidge@users.noreply.github.com>
2025-09-03 14:24:17 +0200

    Update tretmaniDetails.vue
    import { computed } from 'vue'--> import { ref, computed } from 'vue'
    
    dodano:
    import { useTretmaniStore } from "@/stores/projektStore.js"
    import {onAuthStateChanged} from 'firebase/auth'
    import { auth } from '@/firebase.js'
    
    onAuthStateChanged(auth, (currentUser) => {
     if (currentUser) {
     user.value = currentUser;
    
     } else {
     user.value = null;
     }
    }); -->veže se za <a href>
    
    <a v-if="user" href="..." class="...">....</a>-->Spriječava neprijavljenim korisnicima(user==null) naručivanje za termin tretmana

--- src/views/tretmaniDetails.vue
+++ src/views/tretmaniDetails.vue
@@ -1,11 +1,24 @@
 <script setup>
 import { useRoute } from 'vue-router'
-import { computed } from 'vue'
-import { usetretmaniStore } from "@/stores/projektStore.js"
+import { ref, computed } from 'vue'
+import { useTretmaniStore } from "@/stores/projektStore.js"
+import {onAuthStateChanged} from 'firebase/auth'
+import { auth } from '@/firebase.js'
 
-const store = usetretmaniStore()
+const storeT = useTretmaniStore()
 const route = useRoute()
-const tretman = computed(() => store.getById2(route.params.id))
+const tretman = computed(() => storeT.getById2(route.params.id))
+const user=ref(null)
+
+onAuthStateChanged(auth, (currentUser) => {
+ if (currentUser) {
+ user.value = currentUser;
+
+ } else {
+ user.value = null;
+ }
+});
+
 </script>
 
 <template>
@@ -19,4 +32,5 @@ const tretman = computed(() => store.getById2(route.params.id))
   <div v-else>
     <p>Tretman nije pronađen!</p>
   </div>
+<a v-if="user" href="https://docs.google.com/forms/d/e/1FAIpQLSf-PfQg1hO64jKMxPXrskUyo7oXCnRf7qcJtwkA3mNhj7A2Sw/viewform?usp=header" class="border-orange-400 border-3 rounded-lg bg-orange-200 mb-3 p-2">Naruci se</a>
 </template>
