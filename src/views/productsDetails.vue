f4c3b8572bf216e398225581a9de302fba459c93
Sanja Žitnik <153129371+Roundpidge@users.noreply.github.com>
2025-09-03 14:16:26 +0200

    Update productsDetails.vue
    import { useRoute } from 'vue-router' --> import { useRoute, useRouter } from 'vue-router'
    
    import { computed } from 'vue' --> import { ref, computed } from 'vue'
    
    import { useproductsStore } from "@/stores/projektStore.js" --> import { useProductsStore, useKosaricaStore, useUserStore } from "@/stores/projektStore.js"
    
    dodano:
    import {onAuthStateChanged} from 'firebase/auth'
    import { auth } from '@/firebase.js'
    import Swal from 'sweetalert2'
    
    const userStore=useUserStore()
    const cart=useKosaricaStore()
    const router=useRouter()
    const user=ref(null)
    
    const id = computed(() => Number((route.params.id)))
    const proizvod = computed(() => store.getById(id.value)) -->traženje potrebnog id-a za dodavanje baš tog proizvoda kojeg korisnik želi u košarici
    
    const kol=ref(1) -->skraceno od kolicina
    
    function povecajKol(){
    kol.value=Math.max(1, Number(kol.value)+1) -->gleda je li kolicina veća od 1, u slučaju da je kolicina manja od 1 uzima, 1 kao vrijednost. Također može i povećati kolicinu za 1 više, no to je više vezano za <button @click=" dodajuKosaricu()">
    }
    
    function smanjiKol(){
    kol.value=Math.max(1, Number(kol.value)-1)
    }
    
    async function dodajuKosaricu(){
      const p=proizvod.value
      if(!p)return
      const k=Math.max(1, Number(kol.value) || 1)-->sprječava da količina ide u minusu
    
    cart.dodajUKosaricu(proizvod.value.id, kol.value)
    
      const {isConfirmed} = await Swal.fire({
      title: `Dodali ste ${p.naziv} x${k}`,
      icon: 'success',
        showCancelButton: true,
        confirmButtonText: 'Pogledaj košaricu',
        cancelButtonText: 'Nastavi kupovati',
    }) 
    
    if(!isConfirmed) return
      let role
      try {role= await userStore.nadiUlogu?.()} catch{role=undefined}
      if(role==='admin'){
        await router.push({ name: 'adminPage', params:{email: user.value?.email} }) 
      } else{
        await router.push({ name: 'korisnik', params:{email: user.value?.email}, query: { tab: 'kosarica' } } )
      }
      
    }; --> ovaj dio sa !Confirmed traži ulogu korisnika kako bi znala u kojoj košarici treba dodati željene proizvode(za običnog korisnika ili admina), da nema tog dijela onda nebi ti proizvodi igdje završili. 
    
    onAuthStateChanged(auth, (currentUser) => {
     if (currentUser) {
     user.value = currentUser;
    
     } else {
     user.value = null;
     }
    });--> potrebno ovdje, da bismo sprječili neprijavljenim korisnicima mogućnost dodavanja proizvoda u košaricu
    
    
    <div v-if="user">--> ako je null onda se praktički ova funckionalnost ne prikazuje neprijavljenim korisnicima
      <button @click="smanjiKol()">-</button>{{ kol }}<button @click="povecajKol()">+</button> -->klikom dodajemo ili smanjujemo količinu uz pomoć smanjiKol ili povecajKol
    <button @click=" dodajuKosaricu()">Dodaj u košaricu</button>
    </div>

--- src/views/productsDetails.vue
+++ src/views/productsDetails.vue
@@ -1,12 +1,66 @@
+
 <script setup>
-import { useRoute } from 'vue-router'
-import { computed } from 'vue'
-import { useproductsStore } from "@/stores/projektStore.js"
+import { useRoute, useRouter } from 'vue-router'
+import { ref, computed } from 'vue'
+import { useProductsStore, useKosaricaStore, useUserStore } from "@/stores/projektStore.js"
+import {onAuthStateChanged} from 'firebase/auth'
+import { auth } from '@/firebase.js'
+import Swal from 'sweetalert2'
+const store = useProductsStore()
+const userStore=useUserStore()
+const cart=useKosaricaStore()
 
-const store = useproductsStore()
 const route = useRoute()
+const router=useRouter()
+const user=ref(null)
 
-const proizvod = computed(() => store.getById(route.params.id))
+const id = computed(() => Number((route.params.id)))
+const proizvod = computed(() => store.getById(id.value))
+const kol=ref(1)
+
+function povecajKol(){
+kol.value=Math.max(1, Number(kol.value)+1)
+}
+
+function smanjiKol(){
+kol.value=Math.max(1, Number(kol.value)-1)
+}
+
+async function dodajuKosaricu(){
+  const p=proizvod.value
+  if(!p)return
+  const k=Math.max(1, Number(kol.value) || 1)
+
+cart.dodajUKosaricu(proizvod.value.id, kol.value)
+
+  const {isConfirmed} = await Swal.fire({
+  title: `Dodali ste ${p.naziv} x${k}`,
+  icon: 'success',
+    showCancelButton: true,
+    confirmButtonText: 'Pogledaj košaricu',
+    cancelButtonText: 'Nastavi kupovati',
+}) 
+
+if(!isConfirmed) return
+  let role
+  try {role= await userStore.nadiUlogu?.()} catch{role=undefined}
+  if(role==='admin'){
+    await router.push({ name: 'adminPage', params:{email: user.value?.email} }) 
+  } else{
+    await router.push({ name: 'korisnik', params:{email: user.value?.email}, query: { tab: 'kosarica' } } )
+  }
+  
+};
+
+onAuthStateChanged(auth, (currentUser) => {
+ if (currentUser) {
+ user.value = currentUser;
+
+ } else {
+ user.value = null;
+ }
+});
+
 </script>
 
 <template>
@@ -16,6 +70,10 @@ const proizvod = computed(() => store.getById(route.params.id))
         <li>Količina: {{ proizvod.kolicina }}</li>
         <li>Cijena: {{ proizvod.cijena }}</li>
     </ol>
+    
+    <div v-if="user"><button @click="smanjiKol()">-</button>{{ kol }}<button @click="povecajKol()">+</button>
+    <button @click=" dodajuKosaricu()">Dodaj u košaricu</button>
+    </div>
   </div>
   <div v-else>
     <p>Proizvod nije pronađen!</p>
