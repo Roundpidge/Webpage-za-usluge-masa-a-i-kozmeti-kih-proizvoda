3b4f357a74489931ecea611e22eafd3a30cc914a
Sanja Žitnik <153129371+Roundpidge@users.noreply.github.com>
2025-09-11 00:55:26 +0200

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,22 @@
 <script setup>
+import gumb2 from '@/components/gumbVanNavbar.vue'
+import naslov from '@/components/naslov.vue'
+import tekst from '@/components/tekst.vue'
 </script>
 
 <template>
-
-<h1 class="text-5xl text-blue-600">O nama</h1>
-   <h4 class=" text-2xl">Kozmetički salon "No stress" nudi razne
-      terapeutske i masažne usluge, kao i kozmetičke porizvode i proizvode osobne njege</h4>
-      <span class="text-3xl">Kontakt:</span>
-      <ol class="text-2xl">
-         <li >Telefon: 098/999-0989</li>
-         <li >Email: TaraTaric@gmail.com</li>
-         <li >Lokacija:Zagrebačka 16, Pula 52100</li>
-      </ol>
-      <img src="../assets/placeholder.jpeg" class="h-50"></img>
-     
+   <div class="text-center px-6">
+   <naslov>O nama</naslov><br></br>
+   <tekst>
+   <ol>
+   <li><strong>Telefon:</strong> 098/999-0989</li>
+   <li><strong>Email:</strong> TaraTaric@gmail.com</li>
+   <li><strong>Lokacija:</strong> Zagrebačka 16, Pula 52100</li>
+   </ol>
+   </tekst>
+   <br></br>
+   <a href="https://www.google.com/maps/search/Zagreba%C4%8Dka+16,+Pula+52100/@44.8624686,13.8310684,14z/data=!3m1!4b1?entry=ttu&g_ep=EgoyMDI1MDkwMy4wIKXMDSoASAFQAw%3D%3D"><gumb2>Put do nas</gumb2></a>
+   </div>
 </template>
 
 <style scoped></style>
