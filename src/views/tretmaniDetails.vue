e5530d2ed7ba322cce72965b7c99a0596527634b
Sanja Žitnik <153129371+Roundpidge@users.noreply.github.com>
2025-09-11 00:58:22 +0200

    Update tretmaniDetails.vue

--- src/views/tretmaniDetails.vue
+++ src/views/tretmaniDetails.vue
@@ -4,11 +4,33 @@ import { ref, computed } from 'vue'
 import { useTretmaniStore } from "@/stores/projektStore.js"
 import {onAuthStateChanged} from 'firebase/auth'
 import { auth } from '@/firebase.js'
+import placeholder from '@/assets/placeholder.jpeg'
+import naslov from '@/components/naslov.vue'
+import tekst from '@/components/tekst.vue'
+import gumb2 from '@/components/gumbVanNavbar.vue'
 
 const storeT = useTretmaniStore()
 const route = useRoute()
 const tretman = computed(() => storeT.getById2(route.params.id))
 const user=ref(null)
+const isAdmin=ref(false);
+
+onAuthStateChanged(auth, async (currentUser) => {
+  user.value = currentUser
+
+  if (currentUser) {
+    try { await reload(currentUser) } catch {}
+    
+    try{ 
+    const role = await storeUser.nadiUlogu()
+    isAdmin.value = role === 'admin'
+  }catch{isAdmin.value=false}
+  } else {
+    isAdmin.value = false
+  }
+})
+
+const verified = computed(() => user.value?.emailVerified === true)
 
 onAuthStateChanged(auth, (currentUser) => {
  if (currentUser) {
@@ -23,14 +45,23 @@ onAuthStateChanged(auth, (currentUser) => {
 
 <template>
   <div v-if="tretman">
-    <h1 class="text-3xl">{{ tretman.naziv }}</h1>
+<div class="text-center px-6">
+    <naslov>{{ tretman.naziv }}</naslov>
+    <tekst>
+    <img :src="tretman.slika || placeholder" alt="slika" class="max-h-20 inline-block ml-2" />
+    <br></br>
     <ol>
         <li>Trajanje: {{ tretman.trajanje }}</li>
         <li>Cijena: {{ tretman.cijena }}</li>
     </ol>
+</tekst>
+<br></br>
+    <a v-if="user && verified || isAdmin" href="https://docs.google.com/forms/d/e/1FAIpQLSf-PfQg1hO64jKMxPXrskUyo7oXCnRf7qcJtwkA3mNhj7A2Sw/viewform?usp=header" ><gumb2>Naruci se</gumb2></a>
+<div v-else>Verificirani korisnici mogu rezervirati termin, za verifikaciju nastavite do "Korisnički račun", ukoliko niste prijavljeni prvo odite do "Registracija i prijava" i automatski nakon registracije ili prijave će vas prebaciti na korisnički račun</div>
+</div>
   </div>
   <div v-else>
     <p>Tretman nije pronađen!</p>
   </div>
-<a v-if="user" href="https://docs.google.com/forms/d/e/1FAIpQLSf-PfQg1hO64jKMxPXrskUyo7oXCnRf7qcJtwkA3mNhj7A2Sw/viewform?usp=header" class="border-orange-400 border-3 rounded-lg bg-orange-200 mb-3 p-2">Naruci se</a>
+
 </template>
