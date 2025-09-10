d684aae982542c71dec5cf42f4bef3838ca2b767
Sanja Žitnik <153129371+Roundpidge@users.noreply.github.com>
2025-09-11 00:57:05 +0200

    Update products.vue

--- src/views/products.vue
+++ src/views/products.vue
@@ -1,15 +1,28 @@
 <script setup>
 import { useProductsStore } from "@/stores/projektStore.js"
+import naslov from '@/components/naslov.vue'
+import tekst from '@/components/tekst.vue'
 const store = useProductsStore();
 </script>
+
 <template>
-<div class="border-r border-blue-400">
-  <RouterLink v-for="proizvod in store.proizvodi" :key="proizvod?.id" :to="`/proizvodDetaljno/${proizvod.id}`">
-    <ol class=" w-fit flex justify-center m-2 rounded hover:text-orange-400">
-      <li>{{ proizvod.naziv }} | {{ proizvod.cijena }} €</li>
-    </ol>
-  </RouterLink>
-</div>
-<h1 class="text-gray-200">Propustili ste popuste na tretmane i proizvode? Registracijom ili prijavom svojeg računa možete omogućiti primanje ranih obavijesti o nadolazećim popustima i novostima kako biste bili u toku do danas</h1>
+    <naslov>Proizvodi u ponudi</naslov>
+  <div class="flex justify-center my-6">
+    <table>
+      <tbody>
+        <tr v-for="proizvod in store.proizvodi" :key="proizvod?.id">
+          <td class="p-3 text-center">
+            <tekst>
+              <RouterLink :to="`/proizvodDetaljno/${proizvod.id}`" class="hover:text-[#24b550] font-medium">
+                {{ proizvod.naziv }}
+              </RouterLink>
+              <div class="text-sm text-gray-700">
+                {{ proizvod.cijena }} €
+              </div>
+            </tekst>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
-<style scoped></style>
