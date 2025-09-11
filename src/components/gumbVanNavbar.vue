42f0b22750276c6f265bf55d79df7e8648225e15
Sanja Žitnik <153129371+Roundpidge@users.noreply.github.com>
2025-09-11 15:50:55 +0200

    Update gumbVanNavbar.vue

--- src/components/gumbVanNavbar.vue
+++ src/components/gumbVanNavbar.vue
@@ -1,6 +1,13 @@
 <template>
-  <button
-    class="inline-flex items-center justify-center rounded-3xl mb-2 bg-[#81E15E] hover:text-[#24b550] w-20 h-10 text-sm text-[#1b7511]">
+  <button :type="type" class="inline-flex items-center justify-center rounded-3xl mb-2 bg-[#81E15E] hover:text-[#24b550] w-20 h-10 text-sm text-[#1b7511]"  @click="$emit('click', $event)">
     <slot></slot>
-</button>
+  </button>
 </template>
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    default: 'button' 
+  }
+})
+</script>
